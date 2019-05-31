90e8372787280c27485d880f26c05b6253336bfd
Scott Noel-Hemming <frogstarr78@gmail.com>
2019-05-30 20:06:31 -0700

    Updated the name of the file and the railo references, changing them to lucee

--- make_lucee4cpanel.sh
+++ make_lucee4cpanel.sh
@@ -3,7 +3,7 @@
 ###############################################################################
 #
 # Purpose:      This script builds the self-extracting tar.gz file out of the
-#               railo4cpanel directory and extract.sh script.
+#               lucee4cpanel directory and extract.sh script.
 #
 # Copyright:    Copyright (C) 2012-2013
 #               by Jordan Michaels (jordan@viviotech.net)
@@ -41,17 +41,17 @@ myInstallerVersion="0.1.01"
 ###############################################################################
 
 function check_executing_dir {
-	if [[ ! -d railo4cpanel/ ]]; then
-		echo "* [FATAL] Can't find railo4cpanel/ directory!";
+	if [[ ! -d lucee4cpanel/ ]]; then
+		echo "* [FATAL] Can't find lucee4cpanel/ directory!";
 		echo "Exiting...";
 		exit 1;
 	fi
 }
 
 function check_existing_tgz {
-	if [[ -f railo4cpanel.tgz ]]; then
-		echo -n "* Removing previous railo4cpanel.tgz...";
-		rm -rf railo4cpanel.tgz;
+	if [[ -f lucee4cpanel.tgz ]]; then
+		echo -n "* Removing previous lucee4cpanel.tgz...";
+		rm -rf lucee4cpanel.tgz;
 		echo "[DONE]";
 	fi
 }
@@ -59,29 +59,29 @@ function check_existing_tgz {
 function create_manifest {
 	echo -n "* Building new MD5 File Manifest...";
 	# create the manifest
-	find railo4cpanel/ -type f -print0 | xargs -0 md5sum >> manifest.md5
+	find lucee4cpanel/ -type f -print0 | xargs -0 md5sum >> manifest.md5
 	# move the manifest to the directory to be zipped up with the package
-	mv manifest.md5 railo4cpanel/
+	mv manifest.md5 lucee4cpanel/
 	echo "[DONE]";
 }
 
 function create_tgz {
-	echo -n "* Creating new railo4cpanel.tgz file...";
-	tar -czf railo4cpanel.tgz railo4cpanel/
+	echo -n "* Creating new lucee4cpanel.tgz file...";
+	tar -czf lucee4cpanel.tgz lucee4cpanel/
 	echo "[DONE]";
 }
 
 function create_selfextractor {
 	echo -n "* Building Self-Extractor...";
-	cat extract.sh railo4cpanel.tgz > railo4cpanel-${myInstallerVersion}-installer.run
-	chmod 744 railo4cpanel-${myInstallerVersion}-installer.run
+	cat extract.sh lucee4cpanel.tgz > lucee4cpanel-${myInstallerVersion}-installer.run
+	chmod 744 lucee4cpanel-${myInstallerVersion}-installer.run
 	echo "[DONE]";
 }
 
 function cleanup {
 	echo -n "* Cleaning up...";
-	rm -rf railo4cpanel.tgz;
-	rm -f railo4cpanel/manifest.md5;
+	rm -rf lucee4cpanel.tgz;
+	rm -f lucee4cpanel/manifest.md5;
 	echo "[DONE]";
 }
 
